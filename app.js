7ee735189a183ca5e9313baeb3d7b45105f72011
Daniel Jowett <dbjowett@gmail.com>
2021-08-19 22:11:52 +0900

    Update Font, slider speed, remove Logo and Name

--- app.js
+++ app.js
@@ -23,7 +23,6 @@ tl
     .fromTo(hero, 1.1, {width: "100%"}, {width: "80%", ease: Power2.easeInOut})
     .fromTo(slider, 1.1, {x: "-100%"},{x: "0%", ease: Power2.easeInOut}, "-=1")
     .fromTo(shape, 1.1, {x: "-100%"},{x: "0%", ease: Power2.easeInOut},"-=1.1")
-    .fromTo(logo, 0.5, {opacity:"0", x:"30"},{ opacity: "1", x:"0"},"-=0.3")
     .fromTo(navlist, 0.5, {opacity:"0", x:"-30"},{ opacity: "1", x:"0"},"-=0.5")
     .fromTo(headline, 0.5,{opacity: "0", x:"-100"},{ opacity: "1"},"-=1");
     
